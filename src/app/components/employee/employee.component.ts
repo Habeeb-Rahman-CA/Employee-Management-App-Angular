7183c8697e14bf654848124f73e38844ddd707de
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-02 10:11:07 +0530

    done

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+import { IEmployee } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  
+  employeeService = inject(EmployeeService)
+  employeeList: IEmployee[] = []
 
+  ngOnInit(): void {
+    this.getAllEmployee()
+  }
+
+  getAllEmployee(){
+    this.employeeService.getAllEmployee().subscribe((res) =>{
+      this.employeeList = res.data
+    })
+  }
 }
